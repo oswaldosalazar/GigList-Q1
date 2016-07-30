626095062600731d258327b8f351ba6ebeecdb82
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2016-07-30 17:42:07 -0600

    Reading more than one artists from form

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 $(document).ready(function() {
 
     var formInput = {
-        artist: [],
         city: '',
         state: '',
+        artists: [],
     }
 
     $('#submit').on('click', function() {
           //variables
-          formInput.artist = $('#artistURL').val()
           formInput.city = $('#cityURL').val()
           formInput.state = $('#select').val()
+          formInput.artists = $('#artistURL').val().split(',')
           $(console.log(formInput.city))
           $(console.log(formInput.state))
-          $(console.log(formInput.artist))
+          $(console.log(formInput.artists))
+          $(console.log(formInput.artists[1]))
           $.ajax({
-              url: 'http://api.bandsintown.com/artists/'+formInput.artist+'/events/search.json?api_version=2.0&app_id=GIGLIST&location='+formInput.city+','+formInput.state+'&radius=50',
+              url: 'http://api.bandsintown.com/artists/'+(formInput.artists[1].trim())+'/events/search.json?api_version=2.0&app_id=GIGLIST&location='+formInput.city+','+formInput.state+'&radius=50',
               dataType: 'jsonp',
               error: function(err) {console.error(err)},
               method: 'GET',
