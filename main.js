d65761f4af0744ba274a27452173d1685a75ef8f
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2016-08-03 21:56:24 -0600

    Submit only working after second click

--- main.js
+++ main.js
@@ -1,70 +1,98 @@
-var allEvents = []
-var artistEvents = []
 var dataLength = 0
 var formInput = {}
-var artists =''
+var recommendedList = []
+var recArtist = []
+var uniqueArtists = []
 
 function gigList (city, state, artists) {
-    for ( var j in artists) {
+
+    for (var j in artists) {
         $.ajax({
             url: 'http://api.bandsintown.com/artists/'+artists[j]+'/events/search.json?api_version=2.0&app_id=GIGLIST&location='+city+','+state+'&radius=50',
             dataType: 'jsonp',
             error: function(err) {console.error(err)},
             method: 'GET',
             success: function(data) {
+                //set length variable to prevent "else" if one data point is empty
                 dataLength += data.length
+                // console.log("datalength   " + dataLength)
+                //routine to fill table if favorite artist is touring in the city
                 if (dataLength > 0) {
-                    $("#successFailureMsg").attr("class","text-success").text("Great! Your favorite artists are on tour in your area!")
+                    $("#successFailureMsg").attr("class","text-success").text("Great! Your favorite artists or recommended ones are touring in your area!")
                     $("#tableHeader").html("<tr><th>Artist</th><th>Venue</th><th>City</th><th>State</th><th>Date and Time</th><th>Tickets</th></tr>")
                     for (var i in data) {
                         $(".table").append("<tr><td>"+data[i].artists[0].name+"</td><td>"+data[i].venue.name+"</td><td>"+data[i].venue.city+"</td><td>"+data[i].venue.region+"</td><td>"+data[i].formatted_datetime+"</td><td> <a href="+data[i].ticket_url+" target="+"_blank"+">Ticket Link</a> </td></tr>")
-                        // allEvents.push(artistEvents[i])
                     }
-                    console.log(dataLength)
-                    console.log(artists)
-                } else {
-                    console.log("RECOMMEND COMMENT")
-                    $("#successFailureMsg").empty()
-                    $("#tableHeader tr").empty()
                 }
             },
         })
     }
     dataLength = 0
+
 }
 
-function recommended(artists) {
+
+function recommend (city, state, artists) {
+
     console.log("Recommend for "+artists)
+    for (var j in artists) {
+        $.ajax({
+            url: 'http://api.bandsintown.com/artists/'+(artists[j])+'/events/recommended?location='+city+','+state+'&radius=50&app_id=YOUR_APP_ID&api_version=2.0&format=json',
+            dataType: 'jsonp',
+            error: function(err) {console.error(err)},
+            method: 'GET',
+            success: function(dataRecom) {
+                console.log(dataRecom)
+                if (dataRecom.length > 0) {
+                    for (var i in dataRecom) {
+                        recommendedList.push(dataRecom[i].artists[0].name)
+                    }
+                }
+            },
+        })
+    }
+
+    uniqueArtists = recommendedList.filter(function(elem, pos) {
+        return recommendedList.indexOf(elem) == pos;
+    })
+    console.log(recommendedList.length)
+    if (uniqueArtists.length === 0) {
+        $("#successFailureMsg").empty()
+        $("#tableHeader tr").empty()
+        $("#successFailureMsg").attr("class","text-danger").text("Bummer! Please wait for your favorite artists or recommended ones to tour in your area.")
+    }
+    recommendedList = []
+    console.log("Complete list no repeat: " + uniqueArtists)
+
+    return uniqueArtists
 }
-
-
-
 
 $(document).ready(function() {
 
     $('#submit').on('click', function(event) {
         //Default action prevented
         event.preventDefault()
+        // $("#successFailureMsg").empty()
+        $("#tableHeader tr").empty()
         //Assign variables from form inputs.
         formInput.city = $('#cityURL').val()
         formInput.state = $('#select').val()
         formInput.artists = $('#artistURL').val().split(',')
         //Trim spaces from entered artists names
         formInput.artists = formInput.artists.map(Function.prototype.call, String.prototype.trim)
+        console.log(formInput.artists)
         //Remove data form previous clicks
         $("#eventsTable tr").remove()
-        //Call function to populate table with input artists
-        gigList(formInput.city, formInput.state, formInput.artists)
+        //Call function to populate table with input and recommended artists
+        // gigList(formInput.city, formInput.state, formInput.artists)
+        recArtist = recommend(formInput.city, formInput.state, formInput.artists)
+        console.log(recArtist)
+        gigList(formInput.city, formInput.state, recArtist)
+        // gigList(formInput.city, formInput.state, formInput.artists)
+
+
+        console.log("Recommended array: "+recArtist)
+
         //Call function to recommend artists
-        recommended(formInput.artists)
     })
 })
-
-                        // artistEvents[i] = {
-                        //     tabArtist: data[i].artists[0].name,
-                        //     tabVenue: data[i].venue.name,
-                        //     tabCity: data[i].venue.city,
-                        //     tabState: data[i].venue.region,
-                        //     tabDate: data[i].formatted_datetime,
-                        //     tabTix: data[i].ticket_url,
-                        // }
