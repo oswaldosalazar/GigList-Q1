c72d3d9513dc7ee8d7b6d857042f7fc916890ae9
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2016-08-08 13:17:40 -0600

    Updated icon image

---
+++ main1.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+
+    var allEvents = []
+    var artistEvents = []
+
+    $('#submit').on('click', function(event) {
+        // var dataLength = 0
+        var formInput = {}
+        event.preventDefault()
+        $("#eventsTable tr").remove()
+        formInput.city = $('#cityURL').val()
+        formInput.state = $('#select').val()
+        formInput.artists = $('#artistURL').val().split(',')
+        $(console.log(formInput.city))
+        $(console.log(formInput.state))
+        formInput.artists = formInput.artists.map(Function.prototype.call, String.prototype.trim)
+        $(console.log(formInput.artists))
+        for (var j in formInput.artists) {
+            $.ajax({
+                url: 'http://api.bandsintown.com/artists/'+(formInput.artists[j])+'/events/search.json?api_version=2.0&app_id=GIGLIST&location='+formInput.city+','+formInput.state+'&radius=50',
+                dataType: 'jsonp',
+                error: function(err) {console.error(err)},
+                method: 'GET',
+                success: function(data) {
+                    dataLength = data.length
+                    if (dataLength > 0) {
+                        $("#successFailureMsg").attr("class","text-success").text("Great! Your favorite artists are on tour in your area!")
+                        for (var i in data) {
+                            $(".table").append("<tr><td>"+data[i].artists[0].name+"</td><td>"+data[i].venue.name+"</td><td>"+data[i].venue.city+"</td><td>"+data[i].venue.region+"</td>                        <td>"+data[i].formatted_datetime+"</td><td> <a href="+data[i].ticket_url+" target="+"_blank"+">Ticket Link</a> </td></tr>")
+                            allEvents.push(artistEvents[i])
+                        }
+                        // console.log(dataLength)
+                        dataLength = 0
+                    } else {
+                        $.ajax({
+                            url: 'http://api.bandsintown.com/artists/'+(formInput.artists[j])+'/events/recommended?location='+formInput.city+','+formInput.state+'&radius=50&app_id=YOUR_APP_ID&api_version=2.0&format=json',
+                            dataType: 'jsonp',
+                            error: function(err) {console.error(err)},
+                            method: 'GET',
+                            success: function(dataRecom) {
+                                // console.log(dataLength)
+                                if (dataRecom.length === 0) {
+
+                                    $("#successFailureMsg").attr("class","text-danger").text("Sorry, none of your artists are coming and there are no recommendations at this time. Check out in a few days!")
+                                } else {
+
+                                    $("#successFailureMsg").attr("class","text-warning").text("Bummer! Your favorite artists are not coming but check out these recommendations!")
+                                    for (var i in dataRecom) {
+                                        $(".table").append("<tr><td>"+dataRecom[i].artists[0].name+"</td><td>"+dataRecom[i].venue.name+"</td><td>"+dataRecom[i].venue.city+"</td><td>"+dataRecom[i].venue.region+"</td>                        <td>"+dataRecom[i].formatted_datetime+"</td><td> <a href="+dataRecom[i].ticket_url+" target="+"_blank"+">Ticket Link</a> </td></tr>")
+                                        allEvents.push(artistEvents[i])
+                                    }
+                                }
+
+                            },
+                        })
+                    }
+                    // dataLength = 0
+                },
+            })
+        }
+    })
+})
+
+                        // artistEvents[i] = {
+                        //     tabArtist: data[i].artists[0].name,
+                        //     tabVenue: data[i].venue.name,
+                        //     tabCity: data[i].venue.city,
+                        //     tabState: data[i].venue.region,
+                        //     tabDate: data[i].formatted_datetime,
+                        //     tabTix: data[i].ticket_url,
+                        // }
